4806bf8f2faa546861b6cb8b795d59386c8bd3f3
Ekaterina Mamaeva <katerina.grozdova@mail.ru>
2023-04-28 14:25:57 +0300

    update commit

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -10,8 +10,11 @@ public class Book {
         return "колличество страниц "  + amount;
     }
 
-    public String comparison (Book book1, Book book2){
-        if (book1.amount > book2.amount) return " первая книга больше "; else
-        if (book1.amount < book2.amount) return " вторая книга больше "; else return "книги равны";
+    public String comparison (Book book1, Book book2) {
+        if (book1.amount > book2.amount) {
+            return " первая книга больше ";
+        } else if (book1.amount < book2.amount) {
+            return " вторая книга больше ";
+        }else return "книги равны";
     }
 }
--- src/test/java/BookTests.java
+++ src/test/java/BookTests.java
@@ -2,46 +2,51 @@ import org.example.Book;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class BookTests {
     @Test
-    public void comparison1() {
+    public void test_comparison1() {
+
+        String expected = " первая книга больше ";
         Book book1 = new Book();
         Book book2 = new Book();
         Book sut = new Book();
         book1.amount= 900; book2.amount= 800;
-        boolean expected = book1.amount > book2.amount;
 
-        boolean actual = Boolean.parseBoolean(sut.comparison(book1, book2));
+        String actual = sut.comparison(book1, book2);
 
-        assertTrue(actual);
+        assertEquals(expected,actual);
     }
 
     @Test
-    public void comparison2() {
+    public void test_comparison2() {
+
+        String expected = " вторая книга больше ";
         Book book1 = new Book();
         Book book2 = new Book();
         Book sut = new Book();
         book1.amount= 600; book2.amount= 800;
-        boolean expected = book1.amount < book2.amount;
 
-        boolean actual = Boolean.parseBoolean(sut.comparison(book1, book2));
+        String actual = sut.comparison(book1, book2);
 
-        assertTrue(actual);
+        assertEquals(expected,actual);
     }
 
     @Test
-    public void comparison3() {
+    public void test_comparison3() {
+
+        String expected = "книги равны";
         Book book1 = new Book();
         Book book2 = new Book();
         Book sut = new Book();
-        book1.amount= 800; book2.amount= 800;
-        int expected = book1.amount = book2.amount;
+        book1.amount = 800;
+        book2.amount = 800;
 
-        boolean actual = Boolean.parseBoolean(sut.comparison(book1, book2));
+        String actual = sut.comparison(book1, book2);
 
-        assertTrue(actual);
+        assertEquals(expected, actual);
     }
+
 }
